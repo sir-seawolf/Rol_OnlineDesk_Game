ccdfec9ec627d75b088d9cfb667a778d88d7f9f7
Sir-Seawolf <pascualcaballero@gmail.com>
2022-03-12 00:04:14 +0100

    va mejor GET y POST

--- src/routes/pj.js
+++ src/routes/pj.js
@@ -1,12 +1,32 @@
+const { request } = require('express');
 const { Router } = require('express');
 const router = Router();
 
 
 const personajes = require('../game.json');
-console.log(personajes); 
+//console.log(personajes); 
 
 router.get('/', (req, res) => {
-    res.json('movies');
+    res.json(personajes);
 });
+
+
+router.post('/', (req, res) => {
+    //res.json(personajes);
+    //console.log(req.body);
+    const { game, user, pjs } = req.body;
+    if (game && user && pjs) {
+        const id = personajes.length + 1;
+        const newPersonaje = {id, ...req.body}; 
+        //console.log(newPersonaje);
+        res.json(personajes);
+        personajes.push(newPersonaje);
+    }else{
+        res.send('Wrong request');
+    }
+   
+});
+
+
 
 module.exports = router; 
